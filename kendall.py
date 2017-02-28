9f785b8f155b1b788904055aa21eb9b8b23b2155
Alessandro Checco <checcoa@tcd.ie>
2017-02-28 19:59:26 +0000

    Update kendall.py

--- kendall.py
+++ kendall.py
@@ -4,7 +4,7 @@ This module generalise kendall-tau as defined in [1].
 It returns a distance: 0 for identical (in the sense of top-k) lists and 1 if completely different.
 
 Example:
-    Simply call kendall_top_k with two arrays of ratings (or also rankings) and k and p (default is 0, see [1]) as parameters:
+    Simply call kendall_top_k with two same-length arrays of ratings (or also rankings), length of the top elements k (default is the maximum length possible), and p (default is 0, see [1]) as parameters:
 
         import kendall
         a = np.array([1,2,3,4,5])
@@ -27,7 +27,7 @@ def kendall_top_k(a,b,k=None,p=0): #zero is equal 1 is max distance, compare wit
     It returns a distance: 0 for identical (in the sense of top-k) lists and 1 if completely different.
 
     Example:
-        Simply call it with two arrays of ratings (or also rankings) and k and p (default is 0, see [1]) as parameters:
+        Simply call it with two same-length arrays of ratings (or also rankings), length of the top elements k (default is the maximum length possible), and p (default is 0, see [1]) as parameters:
 
             $ a = np.array([1,2,3,4,5])
             $ b = np.array([5,4,3,2,1])
